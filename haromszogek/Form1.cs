4a33d61a3fdd745810a07ef345eefe43c0290788
previzdaniel <dani.2000.2x@gmail.com>
2020-10-05 12:37:50 +0200

    Add project files.

---
+++ haromszogek/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace haromszogek
+{
+    public partial class frmfo : Form
+    {
+        private int aOldal;
+        private int bOldal;
+        private int cOldal;
+
+        public frmfo()
+        {
+            aOldal = 0;
+            bOldal = 0;
+            cOldal = 0;
+            InitializeComponent();
+            tbAoldal.Text = aOldal.ToString();
+            tbBoldal.Text = bOldal.ToString();
+            tbColdal.Text = cOldal.ToString();
+            lbHaromszogLista.Items.Clear();
+        }
+
+        private void btnKilepes_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSzamol_Click(object sender, EventArgs e)
+        {
+            aOldal = int.Parse(tbAoldal.Text);
+            bOldal = int.Parse(tbBoldal.Text);
+            cOldal = int.Parse(tbColdal.Text);
+
+            StringBuilder szoveg = new StringBuilder();
+            szoveg.Append("a: ");
+            szoveg.Append(aOldal.ToString());
+            szoveg.Append("b: ");
+            szoveg.Append(bOldal.ToString());
+            szoveg.Append("c: ");
+            szoveg.Append(cOldal.ToString());
+
+            if (aOldal == 0 || bOldal == 0 || cOldal == 0)
+            {
+                MessageBox.Show("Egyik oldal sem lehet 0", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                var h = new Haromszog(aOldal, bOldal, cOldal);
+                List<string> adatok = h.AdatokSzoveg();
+                foreach (var a in adatok)
+                {
+                    lbHaromszogLista.Items.Add(a);
+                }
+            }
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            if (lbHaromszogLista.Items.Count > 0)
+            {
+                lbHaromszogLista.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Nincs mit törölni cula!");
+            }
+        }
+    }
+}
---
+++ haromszogek/Haromszog.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace haromszogek
+
+{
+    internal class Haromszog
+    {
+        private int aOldal;
+        private int bOldal;
+        private int cOldal;
+
+
+
+        public double Terulet { get; private set; }
+        public double Kerulet { get; private set; }
+        public bool Szerkesztheto { get; private set; }
+
+        public List<string> AdatokSzoveg()
+        {
+            List<string> adatok = new List<string>();
+            adatok.Add($"a: {aOldal} - b: {bOldal} - c: {cOldal}");
+            
+            if (Szerkesztheto)
+            {
+                adatok.Add($"Kerület: {Kerulet} - Terület: {Terulet}");
+            }
+            else
+            {
+                adatok.Add("Nem szerkeszthető!");
+            }
+            return adatok;
+        }
+
+        private void Szerk()
+        {
+            if (aOldal + bOldal > cOldal && aOldal + cOldal > bOldal && bOldal + cOldal > aOldal)
+            {
+                Szerkesztheto = true;
+                Terulet = TeruletSzamitas();
+                Kerulet = KeruletSzamitas();
+            }
+            else
+            {
+                Szerkesztheto = false;
+                Terulet = 0;
+                Kerulet = 0;
+            }
+        }
+
+        private double TeruletSzamitas()
+        {
+            double s = (aOldal + bOldal + cOldal) / 2;
+            Terulet = System.Math.Sqrt(s * (s - aOldal) * (s - bOldal) * (s - cOldal));
+            return Terulet;
+        }
+
+        private double KeruletSzamitas()
+        {
+            Kerulet = aOldal + bOldal + cOldal;
+            return Kerulet;
+        }
+
+        public Haromszog(int aOldal, int bOldal, int cOldal)
+        {
+            this.aOldal = aOldal;
+            this.bOldal = bOldal;
+            this.cOldal = cOldal;
+            Szerk();
+        }
+    }
+}
